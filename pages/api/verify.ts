e126784c970c35751fabc88b4c8d7a3c24dbb108
vishal <vishalkoolkarni0045@gmail.com>
2025-09-20 10:55:56 +0530

    update endpoint

--- pages/api/verify.ts
+++ pages/api/verify.ts
@@ -56,7 +56,7 @@ export default async function handler(
 
       const selfBackendVerifier = new SelfBackendVerifier(
         "self-playground",
-        "https://playground.self.xyz/api/verify",
+        "https://886793e430a4.ngrok-free.app/api/verify",
         false,
         AllIds,
         configStore,
